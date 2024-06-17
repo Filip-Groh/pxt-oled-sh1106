ee8eb23d8337cfa02439b0d801418c95f032f195
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-06-17 22:49:39 +0200

    updates

--- oled.ts
+++ oled.ts
@@ -98,6 +98,20 @@ namespace SH1106 {
         return data
     }
 
+    export function draw(new_data: Buffer) {
+        startReadModifyWriteMode()
+        let buffer = pins.createBuffer(1)
+        buffer[0] = 0x40
+        pins.i2cWriteBuffer(chipAdress, buffer)
+        let old_data = pins.i2cReadBuffer(chipAdress, new_data.length, false)
+
+        old_data.toArray(NumberFormat.UInt8LE).forEach((value: number, index: number) => {
+            new_data[index] |= value
+        })
+        pins.i2cWriteBuffer(chipAdress, buffer.concat(new_data))
+        stopReadModifyWriteMode()
+    }
+
     export function readStatus(): State {
         let buffer = pins.createBuffer(1)
         buffer[0] = 0x00
@@ -121,7 +135,8 @@ namespace SH1106 {
     }
 
     export function setColumnAddress(address: number) {
-        command(address >> 4 | 0x10)
+        address %= 132
+        command(address >> 4 + 0x10)
         command(address & 0x0F)
     }
 
@@ -154,15 +169,18 @@ namespace SH1106 {
     }
 
     export function setDisplayStartLine(startLine: number) {
+        startLine %= 64
         command(0x40 + startLine)
     }
 
     export function setContrast(contrast: number) {
+        contrast %= 256
         command(0x81)
         command(contrast)
     }
 
     export function setMultiplexRation(multiplexRation: number) {
+        multiplexRation %= 64
         command(0xA8)
         command(multiplexRation)
     }
@@ -180,15 +198,18 @@ namespace SH1106 {
     }
 
     export function setPageAddress(address: number) {
+        address %= 8
         command(0xB0 + address)
     }
 
     export function setDisplayOffset(offset: number) {
+        offset %= 64
         command(0xD3)
         command(offset)
     }
 
     export function setDisplayClock(divideRatio: number, oscillatorPercentile: OscillatorPercentile) {
+        divideRatio %= 16
         command(0xD5)
         command(oscillatorPercentile << 4 + divideRatio)
     }
@@ -204,6 +225,7 @@ namespace SH1106 {
     }
 
     export function setVCOMDeselectLevel(deselectLevel: number) {
+        deselectLevel %= 65
         command(0xDB)
         command(deselectLevel)
     }
@@ -225,281 +247,25 @@ namespace SH1106 {
     }
 }
 
-Display.init()
-Display.clear()
+enum DrawMode {
+    Overwrite,
+    Blend
+}
 
-Display.drawChar(0, 0, "A")
+class Display {
+    private font: Buffer
+    private font_text: string = "00000000003E5B4F5B3E3E6B4F6B3E1C3E7C3E1C183C7E3C181C577D571C1C5E7F5E1C00183C1800FFE7C3E7FF0018241800FFE7DBE7FF30483A060E2629792926407F050507407F05253F5A3CE73C5A7F3E1C1C08081C1C3E7F14227F22145F5F005F5F06097F017F006689956A606060606094A2FFA29408047E040810207E201008082A1C08081C2A08081E101010100C1E0C1E0C30383E3830060E3E0E06000000000000005F00000007000700147F147F14242A7F2A12231308646236495620500008070300001C2241000041221C002A1C7F1C2A08083E080800807030000808080808000060600020100804023E5149453E00427F400072494949462141494D331814127F1027454545393C4A49493141211109073649494936464949291E0000140000004034000000081422411414141414004122140802015909063E415D594E7C1211127C7F494949363E414141227F4141413E7F494949417F090909013E414151737F0808087F00417F41002040413F017F081422417F404040407F021C027F7F0408107F3E4141413E7F090909063E4151215E7F09192946264949493203017F01033F4040403F1F2040201F3F4038403F631408146303047804036159494D43007F4141410204081020004141417F04020102044040404040000307080020545478407F284444383844444428384444287F385454541800087E090218A4A49C787F0804047800447D40002040403D007F1028440000417F40007C047804787C080404783844444438FC1824241818242418FC7C08040408485454542404043F44243C4040207C1C2040201C3C4030403C44281028444C9090907C4464544C4400083641000000770000004136080002010204023C2623263C1EA1A161123A4040207A385454555921555579412154547841215554784020545579400C1E5272123955555559395454545939555454580000457C410002457D420001457C40F0292429F0F0282528F07C545545002054547C547C0A097F4932494949323248484832324A4848303A4141217A3A42402078009DA0A07D39444444393D4040403D3C24FF2424487E4943662B2FFC2F2BFF0929F620C0887E090320545479410000447D413048484A32384040227A007A0A0A727D0D19317D2629292F28262929292630484D4020380808080808080808382F10C8ACBA2F102834FA00007B000008142A142222142A1408AA005500AAAA55AA55AA000000FF00101010FF00141414FF001010FF00FF1010F010F0141414FC001414F700FF0000FF00FF1414F404FC141417101F10101F101F1414141F00101010F0000000001F101010101F10101010F010000000FF101010101010101010FF10000000FF140000FF00FF00001F10170000FC04F414141710171414F404F40000FF00F714141414141414F700F7141414171410101F101F141414F4141010F010F000001F101F0000001F14000000FC140000F010F01010FF10FF141414FF141010101F00000000F010FFFFFFFFFFF0F0F0F0F0FFFFFF0000000000FFFF0F0F0F0F0F38444438447C2A2A3E147E02020606027E027E0263554941633844443C04407E201E2006027E020299A5E7A5991C2A492A1C4C7201724C304A4D4D303048784830BC625A463D3E494949007E0101017E2A2A2A2A2A44445F444440514A444040444A51400000FF0103E080FF000008086B6B083612362436060F090F06000018180000001010003040FF0101001F01011E00191D1712003C3C3C3C0000000000"
 
+    public width: number = 132
+    public height: number = 64
 
+    public drawMode: DrawMode = DrawMode.Blend
 
-
-
-
-namespace Display {
-    const width: number = 132
-    const height: number = 64
-    let font = hex`
-    0000000000
-    3E5B4F5B3E
-    3E6B4F6B3E
-    1C3E7C3E1C
-    183C7E3C18
-    1C577D571C
-    1C5E7F5E1C
-    00183C1800
-    FFE7C3E7FF
-    0018241800
-    FFE7DBE7FF
-    30483A060E
-    2629792926
-    407F050507
-    407F05253F
-    5A3CE73C5A
-    7F3E1C1C08
-    081C1C3E7F
-    14227F2214
-    5F5F005F5F
-    06097F017F
-    006689956A
-    6060606060
-    94A2FFA294
-    08047E0408
-    10207E2010
-    08082A1C08
-    081C2A0808
-    1E10101010
-    0C1E0C1E0C
-    30383E3830
-    060E3E0E06
-    0000000000
-    00005F0000
-    0007000700
-    147F147F14
-    242A7F2A12
-    2313086462
-    3649562050
-    0008070300
-    001C224100
-    0041221C00
-    2A1C7F1C2A
-    08083E0808
-    0080703000
-    0808080808
-    0000606000
-    2010080402
-    3E5149453E
-    00427F4000
-    7249494946
-    2141494D33
-    1814127F10
-    2745454539
-    3C4A494931
-    4121110907
-    3649494936
-    464949291E
-    0000140000
-    0040340000
-    0008142241
-    1414141414
-    0041221408
-    0201590906
-    3E415D594E
-    7C1211127C
-    7F49494936
-    3E41414122
-    7F4141413E
-    7F49494941
-    7F09090901
-    3E41415173
-    7F0808087F
-    00417F4100
-    2040413F01
-    7F08142241
-    7F40404040
-    7F021C027F
-    7F0408107F
-    3E4141413E
-    7F09090906
-    3E4151215E
-    7F09192946
-    2649494932
-    03017F0103
-    3F4040403F
-    1F2040201F
-    3F4038403F
-    6314081463
-    0304780403
-    6159494D43
-    007F414141
-    0204081020
-    004141417F
-    0402010204
-    4040404040
-    0003070800
-    2054547840
-    7F28444438
-    3844444428
-    384444287F
-    3854545418
-    00087E0902
-    18A4A49C78
-    7F08040478
-    00447D4000
-    2040403D00
-    7F10284400
-    00417F4000
-    7C04780478
-    7C08040478
-    3844444438
-    FC18242418
-    18242418FC
-    7C08040408
-    4854545424
-    04043F4424
-    3C4040207C
-    1C2040201C
-    3C4030403C
-    4428102844
-    4C9090907C
-    4464544C44
-    0008364100
-    0000770000
-    0041360800
-    0201020402
-    3C2623263C
-    1EA1A16112
-    3A4040207A
-    3854545559
-    2155557941
-    2154547841
-    2155547840
-    2054557940
-    0C1E527212
-    3955555559
-    3954545459
-    3955545458
-    0000457C41
-    0002457D42
-    0001457C40
-    F0292429F0
-    F0282528F0
-    7C54554500
-    2054547C54
-    7C0A097F49
-    3249494932
-    3248484832
-    324A484830
-    3A4141217A
-    3A42402078
-    009DA0A07D
-    3944444439
-    3D4040403D
-    3C24FF2424
-    487E494366
-    2B2FFC2F2B
-    FF0929F620
-    C0887E0903
-    2054547941
-    0000447D41
-    3048484A32
-    384040227A
-    007A0A0A72
-    7D0D19317D
-    2629292F28
-    2629292926
-    30484D4020
-    3808080808
-    0808080838
-    2F10C8ACBA
-    2F102834FA
-    00007B0000
-    08142A1422
-    22142A1408
-    AA005500AA
-    AA55AA55AA
-    000000FF00
-    101010FF00
-    141414FF00
-    1010FF00FF
-    1010F010F0
-    141414FC00
-    1414F700FF
-    0000FF00FF
-    1414F404FC
-    141417101F
-    10101F101F
-    1414141F00
-    101010F000
-    0000001F10
-    1010101F10
-    101010F010
-    000000FF10
-    1010101010
-    101010FF10
-    000000FF14
-    0000FF00FF
-    00001F1017
-    0000FC04F4
-    1414171017
-    1414F404F4
-    0000FF00F7
-    1414141414
-    1414F700F7
-    1414141714
-    10101F101F
-    141414F414
-    1010F010F0
-    00001F101F
-    0000001F14
-    000000FC14
-    0000F010F0
-    1010FF10FF
-    141414FF14
-    1010101F00
-    000000F010
-    FFFFFFFFFF
-    F0F0F0F0F0
-    FFFFFF0000
-    000000FFFF
-    0F0F0F0F0F
-    3844443844
-    7C2A2A3E14
-    7E02020606
-    027E027E02
-    6355494163
-    3844443C04
-    407E201E20
-    06027E0202
-    99A5E7A599
-    1C2A492A1C
-    4C7201724C
-    304A4D4D30
-    3048784830
-    BC625A463D
-    3E49494900
-    7E0101017E
-    2A2A2A2A2A
-    44445F4444
-    40514A4440
-    40444A5140
-    0000FF0103
-    E080FF0000
-    08086B6B08
-    3612362436
-    060F090F06
-    0000181800
-    0000101000
-    3040FF0101
-    001F01011E
-    00191D1712
-    003C3C3C3C
-    0000000000`
-
-    export function init() {
-        SH1106.displayOff()      
+    public constructor () {
+        SH1106.displayOff()
         SH1106.setMultiplexRation(0x3F)
         SH1106.setDisplayOffset(0)
-        SH1106.setDisplayStartLine(0)  
+        SH1106.setDisplayStartLine(0)
         SH1106.setPageAddress(0)
         SH1106.setColumnAddress(0)
         SH1106.setVoltagePump(SH1106.PumpVoltage.V800)
@@ -514,26 +280,47 @@ namespace Display {
         SH1106.setCommonPadsHardwareConfiguration(SH1106.PadConfiguration.Alternative)
         SH1106.setVCOMDeselectLevel(0x35)
         SH1106.displayOn()
+
+        this.processFont()
     }
 
-    export function clear() {
-        for(let page: number = 0; page < 8; page++) {
-            let buffer = pins.createBuffer(width)
-            buffer.fill(0x00, 0, width)
-            SH1106.setPageAddress(page)
-            SH1106.setColumnAddress(0)
-            SH1106.write(buffer)
+    private processFont() {
+        this.font = pins.createBuffer(this.font_text.length / 2)
+        for (let i: number = 0; i < this.font_text.length; i += 2) {
+            this.font.setNumber(NumberFormat.UInt8BE, i / 2, parseInt(this.font_text.substr(i, 2), 16))
         }
     }
 
-    export function drawHLine(x: number, y: number, length: number) {
+    public clear() {
+        SH1106.setColumnAddress(0)
+        for (let page: number = 0; page < 8; page++) {
+            let buffer = pins.createBuffer(this.width)
+            buffer.fill(0x00, 0, this.width)
+            SH1106.setPageAddress(page)
+            this.drawCall(buffer, DrawMode.Overwrite)
+        }
+    }
+
+    public drawPoint(x: number, y: number) {
+        SH1106.setColumnAddress(x)
+        SH1106.setPageAddress(Math.floor(y / 8))
+        let buffer = pins.createBuffer(1)
+        buffer[0] = 2 ** (y % 8)
+        this.drawCall(buffer, this.drawMode)
+    }
+
+    public drawHLine(x: number, y: number, length: number) {
         SH1106.setPageAddress(Math.floor(y / 8))
         SH1106.setColumnAddress(x)
         let buffer = pins.createBuffer(length)
         buffer.fill(2 ** (y % 8), 0, length)
-        SH1106.write(buffer)
+        this.drawCall(buffer, this.drawMode)
     }
 
+    public drawVLine(x: number, y: number, length: number) {
+        for (let currentY: number = y; currentY < y + length; currentY++) {
+            this.drawPoint(x, currentY)
+        }
     // export function drawVLine(x: number, y: number, length: number) {
     //     for (let page: number = Math.ceil(y / 8); page < Math.ceil((length + y) / 8); page++) {
     //         SH1106.setPageAddress(page)
@@ -553,10 +340,63 @@ namespace Display {
     //         SH1106.write(buffer)
     //     }
     // }
+    }
 
-    function drawShape(pixels: Array<Array<number>>) {
-        let x1 = width
-        let y1 = height
+    public drawLine(x0: number, y0: number, x1: number, y1: number) {
+        let pixels: Array<Array<number>> = []
+        let kx: number, ky: number, c: number, i: number, xx: number, yy: number, dx: number, dy: number;
+        let targetX = x1
+        let targetY = y1
+        x1 -= x0; kx = 0; if (x1 > 0) kx = +1; if (x1 < 0) { kx = -1; x1 = -x1; } x1++;
+        y1 -= y0; ky = 0; if (y1 > 0) ky = +1; if (y1 < 0) { ky = -1; y1 = -y1; } y1++;
+        if (x1 >= y1) {
+            c = x1
+            for (i = 0; i < x1; i++, x0 += kx) {
+                pixels.push([x0, y0])
+                c -= y1; if (c <= 0) { if (i != x1 - 1) pixels.push([x0 + kx, y0]); c += x1; y0 += ky; if (i != x1 - 1) pixels.push([x0, y0]); }
+                if (pixels.length > 20) {
+                    this.drawShape(pixels)
+                    pixels = []
+                    this.drawLine(x0, y0, targetX, targetY)
+                    return
+                }
+            }
+        } else {
+            c = y1
+            for (i = 0; i < y1; i++, y0 += ky) {
+                pixels.push([x0, y0])
+                c -= x1; if (c <= 0) { if (i != y1 - 1) pixels.push([x0, y0 + ky]); c += y1; x0 += kx; if (i != y1 - 1) pixels.push([x0, y0]); }
+                if (pixels.length > 20) {
+                    this.drawShape(pixels)
+                    pixels = []
+                    this.drawLine(x0, y0, targetX, targetY)
+                    return
+                }
+            }
+        }
+        this.drawShape(pixels)
+    }
+
+    public drawRectangle(x0: number, y0: number, x1: number, y1: number) {
+        this.drawHLine(x0, y0, x1 - x0)
+        this.drawHLine(x0, y1, x1 - x0)
+        this.drawLine(x0, y0, x0, y1)
+        this.drawLine(x1, y0, x1, y1)
+    }
+
+    public writeString(x: number, page: number, str: string) {
+        for (let i = 0; i < str.length; i++) {
+            if (x > this.width - 6) {
+                page++
+            }
+            this.drawChar(x, page, str.charAt(i))
+            x += 6
+        }
+    }
+
+    private drawShape(pixels: Array<Array<number>>) {
+        let x1 = this.width
+        let y1 = this.height
         let x2 = 0
         let y2 = 0
         for (let i = 0; i < pixels.length; i++) {
@@ -588,82 +428,42 @@ namespace Display {
                 }
                 if (line[0] !== 0x00) {
                     SH1106.setColumnAddress(x)
-                    // command(SSD1306_SETCOLUMNADRESS)
-                    // command(x)
-                    // command(x + 1)
                     SH1106.setPageAddress(page)
-                    // command(SSD1306_SETPAGEADRESS)
-                    // command(page)
-                    // command(page + 1)
-                    SH1106.write(line)
+                    this.drawCall(line, this.drawMode)
                 }
             }
         }
     }
 
-    export function drawLine(x0: number, y0: number, x1: number, y1: number) {
-        let pixels: Array<Array<number>> = []
-        let kx: number, ky: number, c: number, i: number, xx: number, yy: number, dx: number, dy: number;
-        let targetX = x1
-        let targetY = y1
-        x1 -= x0; kx = 0; if (x1 > 0) kx = +1; if (x1 < 0) { kx = -1; x1 = -x1; } x1++;
-        y1 -= y0; ky = 0; if (y1 > 0) ky = +1; if (y1 < 0) { ky = -1; y1 = -y1; } y1++;
-        if (x1 >= y1) {
-            c = x1
-            for (i = 0; i < x1; i++, x0 += kx) {
-                pixels.push([x0, y0])
-                c -= y1; if (c <= 0) { if (i != x1 - 1) pixels.push([x0 + kx, y0]); c += x1; y0 += ky; if (i != x1 - 1) pixels.push([x0, y0]); }
-                if (pixels.length > 20) {
-                    drawShape(pixels)
-                    pixels = []
-                    drawLine(x0, y0, targetX, targetY)
-                    return
-                }
-            }
-        } else {
-            c = y1
-            for (i = 0; i < y1; i++, y0 += ky) {
-                pixels.push([x0, y0])
-                c -= x1; if (c <= 0) { if (i != y1 - 1) pixels.push([x0, y0 + ky]); c += y1; x0 += kx; if (i != y1 - 1) pixels.push([x0, y0]); }
-                if (pixels.length > 20) {
-                    drawShape(pixels)
-                    pixels = []
-                    drawLine(x0, y0, targetX, targetY)
-                    return
-                }
-            }
-        }
-        drawShape(pixels)
-    }
-
-    export function drawRectangle(x0: number, y0: number, x1: number, y1: number) {
-        drawLine(x0, y0, x1, y0)
-        drawLine(x0, y1, x1, y1)
-        drawLine(x0, y0, x0, y1)
-        drawLine(x1, y0, x1, y1)
-    }
-
-    export function drawChar(x: number, page: number, char: string) {
+    private drawChar(x: number, page: number, char: string) {
         SH1106.setColumnAddress(x)
         SH1106.setPageAddress(page)
         let line = pins.createBuffer(6)
         for (let i = 0; i < 5; i++) {
             let charIndex = char.charCodeAt(0)
-            let charNumber = font.getNumber(NumberFormat.UInt8BE, 5 * charIndex + i)
-            console.logValue("charNumber", charNumber)
+            let charNumber = this.font.getNumber(NumberFormat.UInt8BE, 5 * charIndex + i)
             line[i] = charNumber
         }
         line[5] = 0x00
-        SH1106.write(line)
+        this.drawCall(line, this.drawMode)
     }
 
-    export function writeString(x: number, page: number, str: string) {
-        for (let i = 0; i < str.length; i++) {
-            if (x > width - 6) {
-                page++
-            }
-            drawChar(x, page, str.charAt(i))
-            x += 6
+    private drawCall(data: Buffer, mode: DrawMode) {
+        switch(mode) {
+            case DrawMode.Overwrite: 
+                SH1106.write(data)
+                break
+            case DrawMode.Blend:
+                SH1106.draw(data)
+                break
         }
     }
 }
+
+let display: Display = new Display()
+display.clear()
+display.drawVLine(10, 10, 40)
+// display.drawRectangle(5, 5, 132 - 5, 64 - 5)
+// display.drawLine(5, 5, 132 - 5, 64 - 5)
+// display.writeString(10, 10, "Hello world!")
+// display.drawPoint(20, 40)
